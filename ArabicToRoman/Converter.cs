3c938d763c105c32d9d9cb772e93717d92cc2d57
deloppa <97598343+deloppa@users.noreply.github.com>
2024-02-23 22:38:49 +0200

    Add project files.

---
+++ ArabicToRoman/Converter.cs
@@ -0,0 +1,15 @@
+﻿namespace ArabicToRoman;
+
+public static class Converter
+{
+    public static string ConvertArabicToRoman(int number)
+    {
+        if(number <= 0 || number > 3999999) return 
+
+
+        if(number == 1) return "I";
+        if (number == 2) return "II";
+        if (number == 3) return "III";
+        return "";
+    }
+}
---
+++ ArabicToRomanTests/ConverterTests.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ArabicToRoman;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArabicToRoman.Tests;
+
+[TestClass()]
+public class ConverterTests
+{
+    [TestMethod()]
+    public void ConvertArabicToRomanTest()
+    {
+        Assert.AreEqual("I", Converter.ConvertArabicToRoman(1));
+    }
+}
